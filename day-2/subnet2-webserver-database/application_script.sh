b0798334412c428f9304275db04f1cf79dfdc627
zeg22 <120732048+zeg22@users.noreply.github.com>
2023-01-26 16:53:26 +0000

    adding ignore

--- day-2/subnet2-webserver-database/application_script.sh
+++ day-2/subnet2-webserver-database/application_script.sh
@@ -10,5 +10,5 @@ git clone https://github.com/zeg22/capstone-web-app.git
 cd capstone-web-app/flask-example-cicd
 
 sudo docker build . -t flask-example-cicd:latest
-
+# shellcheck disable=SC2154
 sudo docker run --rm -d -p 8080:8080/tcp -e "DB_IP=${db_ip}"  -e "DB_USERNAME=${db_username}" -e "DB_PASSWORD=${db_password}" --name flask-example flask-example-cicd:latest

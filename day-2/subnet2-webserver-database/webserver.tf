b9e3b86125f6bc567ce9ed67916d3cc5f4df702e
zeg22 <120732048+zeg22@users.noreply.github.com>
2023-01-26 15:47:58 +0000

    remove ssh key

--- day-2/subnet2-webserver-database/webserver.tf
+++ day-2/subnet2-webserver-database/webserver.tf
@@ -22,10 +22,7 @@ resource "google_compute_instance" "webserver" {
       nat_ip = google_compute_address.webserver_ip.address
     }
   }
-  metadata = {
-    ssh-keys = "testUser:ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDkKntYUWfaapQjqLKXxufcEPkBagqV2tBs+xwJqgoBaOOAExICKRRNn8zdtMRAtlOw+n2TM6AqtrLx4ks/H90QWJVgUeSwIZc516VSHv1GwIIRc5wZIwLkHIAQono5sgaVgA0VLGMBRF0JO0RSr2Sg8Wa1TJ1r6vpC1va99DPqYV8neZ3tlSu84SdyI2W1nxAkFNfOk+5gRWUvhAH8cnlQnRlbZx0RNGRSTQ2YvPN1LfV4Cv37vYPV0ueUMdl7FIxfRCkx8EE3b7qlX0rHhFQfPGvybUtyPCpVWZfznxD2XrRn0uBylxysC7zeywaH30VB8n5HpbooNt1LuK1P3J5l testUser"
-  }
-
+  
   metadata_startup_script = templatefile("./application_script.sh", {db_ip = google_sql_database_instance.postgres.public_ip_address, db_username = google_sql_user.user.name, db_password = random_password.db_password.result})
 
   depends_on = [
